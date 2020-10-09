8ffa44a1d1a56ab25d62cddbc7141bbd46067649
Manuel Desole <46155957+madnol@users.noreply.github.com>
2020-10-09 10:37:18 +0200

    exercise

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+let bingoBoard = document.querySelector('#bingoBoard');
 
-for (let i = 1; i<=76; i++ {
-    // genera un elemento html appartenente alla board 
-    let we = document.querySelector('div');
+// BINGO BOARD
+//This generates the Bingo Board with numbers from 1 to 76
+
+const generateBoard = () => {
+  for (let i = 1; i <= 76; i++) {
+    let div = document.createElement('div');
+    div.classList.add(i);
+    div.innerHTML = `<h3>${i}</h3>`;
+    bingoBoard.appendChild(div);
   }
+};
+
+// PLAYER CARD(S)
+//the generateCard function is called when I load the browser and then if I want to add extra cards,every time I create a new card with the + button.
+const generateCard = () => {
+  let newCard = 
+};
